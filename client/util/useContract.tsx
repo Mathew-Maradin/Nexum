a5dc6654ef60259f12d7ec371ce776f0bf3c6a50
David Li <david.li3100@gmail.com>
2023-05-21 02:00:23 -0400

    new contract

--- client/util/useContract.tsx
+++ client/util/useContract.tsx
@@ -2,7 +2,7 @@ import { createAlchemyWeb3 } from "@alch/alchemy-web3";
 import ABIObject from "../contract.json";
 
 export const useContract = () => {
-  const CONTRACT_ADDY = "0x296C2744b9C20DF4d63817614f3aB25D4d285BF9";
+  const CONTRACT_ADDY = "0xCD02ea426917F4e6A888702D69357E9ecCc55811";
   const web3 = createAlchemyWeb3(process.env.NEXT_PUBLIC_ALCHEMY_URL as string);
   const contract = new web3.eth.Contract(ABIObject.abi, CONTRACT_ADDY);
 
